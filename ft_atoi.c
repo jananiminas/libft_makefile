ea1ddf17a65ced85efc9a678dbfdb3deb418bf0d
jananiminas <jananias323@gmail.com>
2025-08-01 16:42:09 +0400

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -14,17 +14,15 @@
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	sign;
-	int	res;
+	int		i;
+	int		sign;
+	long	res;
 
 	i = 0;
 	sign = 1;
 	res = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
-	{
 		i++;
-	}
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
@@ -34,6 +32,10 @@ int	ft_atoi(const char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = res * 10 + (str[i] - '0');
+		if ((sign == 1) && (res > 2147483648))
+			return (-1);
+		else if ((sign == -1) && (-res < -2147483648))
+			return (0);
 		i++;
 	}
 	return (res * sign);
