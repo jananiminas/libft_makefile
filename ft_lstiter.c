714e13c4d8dc929ce02bb511ac1c2d28126b09c5
jananiminas <jananias323@gmail.com>
2025-08-01 14:54:52 +0400

    Update ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -20,3 +20,27 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst->next;
 	}
 }
+
+// void	print_uppercase(void *content)
+// {
+// 	char	*s = (char *)content;
+// 	while (*s)
+// 	{
+// 		char c = ft_toupper(*s);
+// 		write(1, &c, 1);
+// 		s++;
+// 	}
+// 	write(1, "\n", 1);
+// }
+//
+// int main(void)
+// {
+// 	t_list *a = ft_lstnew("hello");
+// 	t_list *b = ft_lstnew("libft");
+// 	a->next = b;
+//
+// 	ft_lstiter(a, print_uppercase);
+//
+// 	ft_lstclear(&a, NULL);
+// 	return (0);
+// }
