8dbf3b7f4ea6025c2eecb09d38ef99d55c23ff27
jananiminas <jananias323@gmail.com>
2025-08-01 14:47:24 +0400

    Update ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -18,3 +18,10 @@ int	ft_isdigit(int c)
 		return (1);
 	return (0);
 }
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_isdigit('3'));
+// 	printf("%d\n", ft_isdigit('j'));
+// 	printf("%d\n", ft_isdigit(' '));
+// }
