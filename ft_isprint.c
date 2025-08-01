be90f3b8514115a9f53c5f5839c11b577e293007
jananiminas <jananias323@gmail.com>
2025-08-01 14:48:27 +0400

    Update ft_isprint.c

--- ft_isprint.c
+++ ft_isprint.c
@@ -18,3 +18,10 @@ int	ft_isprint(int c)
 		return (1);
 	return (0);
 }
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_isprint('\n'));
+// 	printf("%d\n", ft_isprint('3'));
+// 	printf("%d\n", ft_isprint(' '));
+// }
